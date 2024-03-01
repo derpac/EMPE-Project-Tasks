b38c7e745930b53e10ccbe957efdb9031b507282
Derry Packer <derry.packer2@gmail.com>
2024-03-01 12:53:20 +0000

    add task 3

---
+++ Task3Real/Task3Real.ino
@@ -0,0 +1,51 @@
+
+int Modulationfrequency = 30; // Set modulation frequency
+#define Frequencyratio 100 // Set ratio between modulation and carrier frequency 
+int Carrierfrequency = Modulationfrequency * Frequencyratio; // and calculate carrier frequency
+// Timings and voltages for 1 modulation cycle
+int Ta[Frequencyratio];
+int Tb[Frequencyratio];
+float Vout[Frequencyratio];
+float Vhigh = 1.5;
+float Vlow = -1.5;
+
+
+
+void setup() {
+  // Initialise output pins
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  // Calculate voltages for triangular wave
+  for(int i = 0; i<Frequencyratio/2; i++)
+  {
+      Vout[i]= Vout[Frequencyratio-i]= Vlow+i*(Vhigh-Vlow)/(Frequencyratio/2);
+  }
+  // Calculating Duty cycle from voltage and timings in microseconds
+  for(int i = 0; i<Frequencyratio; i++)
+  {
+    float Duty = ((Vout[i]/5)+1)/2;
+    Ta[i] = 1000000/Carrierfrequency * Duty;
+    Tb[i] = 1000000/Carrierfrequency * (1 - Duty); 
+  }
+}
+
+
+void loop() {
+  // Generate output from timings
+  for(int i = 0;i<Frequencyratio;i++)
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(1, HIGH); 
+    digitalWrite(4, HIGH);
+    delayMicroseconds(Ta[i]);                    
+    digitalWrite(1, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(2, HIGH); 
+    digitalWrite(3, HIGH);  
+    delayMicroseconds(Tb[i]);
+  }
+}
+  
